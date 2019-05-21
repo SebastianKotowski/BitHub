95ab059b19643f3ea24b06a40df46377c0dbd8ab
Sebastian Kotowski <sebastian.kotowski@sdpk.pl>
2019-05-21 16:38:18 +0200

    Add bit form

---
+++ BitHub/ViewModels/BitFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BitHub.Models;
+using System.Collections.Generic;
+
+namespace BitHub.ViewModels
+{
+    public class BitFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
